df66646648586bb2440a39bb3c89aea280add30f
dhairyaj <dhairya.jain.work@gmail.com>
2020-06-23 12:37:45 +0530

    8 directional basic movement added

--- main.py
+++ main.py
@@ -21,8 +21,12 @@ playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 370
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+#to capture the movement speed
+playerX_change = 0
+playerY_change = 0
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 #Game loop
 running = True
@@ -32,12 +36,32 @@ while running:
     screen.fill((255, 255, 255))
     
     for event in pygame.event.get():
+        
+        #To check whether the window is quitted or not
         if event.type == pygame.QUIT:
             running = False
+            
+        #if key is pressed, check whether it's right, left, up or down
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.1
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.1
+            if event.key == pygame.K_UP:
+                playerY_change = -0.1
+            if event.key == pygame.K_DOWN:
+                playerY_change = 0.1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+            if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                playerY_change = 0
     
     
     #Call the player
-    player()
+    playerX += playerX_change
+    playerY += playerY_change
+    player(playerX, playerY)
     
     #Update the window
     pygame.display.update()
