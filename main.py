469af1165ed27083dc7fc6c2ae02d4dc1d73cb3b
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-06-23 17:48:54 +0530

    Change screen size, and set speed

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import pygame
 pygame.init()
 
 #create a screen
-screen = pygame.display.set_mode((800, 800))
+screen = pygame.display.set_mode((1200, 700))
 
 #Title
 pygame.display.set_caption("Chimichangas")
@@ -25,43 +25,46 @@ playerY = 370
 playerX_change = 0
 playerY_change = 0
 
+# Speed
+speed = 3
+
 def player(x, y):
     screen.blit(playerImg, (x, y))
 
 #Game loop
 running = True
 while running:
-    
+
     #First the screen is filled so that every thing is above the screen
     screen.fill((255, 255, 255))
-    
+
     for event in pygame.event.get():
-        
+
         #To check whether the window is quitted or not
         if event.type == pygame.QUIT:
             running = False
-            
+
         #if key is pressed, check whether it's right, left, up or down
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.1
+                playerX_change = -speed
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.1
+                playerX_change = speed
             if event.key == pygame.K_UP:
-                playerY_change = -0.1
+                playerY_change = -speed
             if event.key == pygame.K_DOWN:
-                playerY_change = 0.1
+                playerY_change = speed
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_change = 0
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                 playerY_change = 0
-    
-    
+
+
     #Call the player
     playerX += playerX_change
     playerY += playerY_change
     player(playerX, playerY)
-    
+
     #Update the window
     pygame.display.update()
