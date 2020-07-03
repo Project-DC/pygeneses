e0b7978508c70904ddd8d7757192d9f1826fb711
Aayush Agarwal <aayush.88@outlook.com>
2020-07-03 20:42:14 +0530

    Added Player Log Generator
    Generates human readable scripts of the player logs.

---
+++ player_log_gen.py
@@ -0,0 +1,28 @@
+import os
+path = os.getcwd()
+path = path+'/Players_Data'
+for file in os.listdir(path):
+    file = open("Players_Data/0-0.txt", "r")
+    if file.mode == 'r':
+        contents = file.readlines()
+
+    for line in contents:
+        entity = list()
+        log = line[1:len(line)-3]
+        entity = log.split(',')
+
+        if (entity[0]=='10'):
+            print(f"The player performed action {entity[0]} at time {entity[1]} and earned the reward of {entity[2]} and its energy is {entity[3]}. The number of offspring produced is {entity[4]}.")
+
+        if (entity[0]=='11'):
+            print(f"The player performed action {entity[0]} at time {entity[1]} and earned the reward of {entity[2]} and its energy is {entity[3]}. The number of offspring produced is {entity[4]} and the mate of player is {entity[-1]}")
+
+        if (entity[0]=='12'):
+            print(f"The player performed action {entity[0]} at time {entity[1]} and earned the reward of {entity[2]} and its energy is {entity[3]}. The player fought with {entity[4]}")
+
+        if 'Failed' in entity[0]:
+            print(f"The player failed to perform the action {entity[0][8:-1]} at time {entity[1]} and earned the reward of {entity[2]} and its energy is {entity[3]}.")
+        
+        else:
+            print(f"The player performed action {entity[0]} at time {entity[1]} and earned the reward of {entity[2]} and its energy is {entity[3]}")
+        
