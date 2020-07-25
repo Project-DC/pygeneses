375788f83573c927c10b6fad1022b63c3c4e154d
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-07-26 02:34:06 +0530

    Add basic structure of vitaboard package

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="pyevolution",
-    version="0.1",
+    version="0.1.0",
     description="PyTorch based framework for training artificial agents in bio-inspired environments",
     long_description=README,
     long_description_content_type="text/markdown",
