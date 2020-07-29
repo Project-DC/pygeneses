d2c7c849cbc45e2b5da72d87f5da6ed4e1f68a6c
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-07-30 01:20:54 +0530

    Update profiling code

--- profile.py
+++ profile.py
@@ -15,7 +15,7 @@ def training_loop(n_episodes=1000, max_t=1000, gamma=1.0, print_every=100):
     running = True
     while running:
         rl_model.update_time()
-        if(rl_model.time == 100):
+        if(rl_model.time == 60):
             running = False
         elif(rl_model.time % 10 == 0):
             print("\U0001F552 Time:", rl_model.time)
