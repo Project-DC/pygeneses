0c7001863e40242e6ea4563b103e8578451f9df4
frankhart2018 <siddharthadhar_soumen@srmuniv.edu.in>
2020-08-16 01:37:21 +0530

    Add comments in hypertune package

--- pygeneses/hypertune/hypertune.py
+++ pygeneses/hypertune/hypertune.py
@@ -1,16 +1,53 @@
+# Hyperparameter tuning package
+
+# Import required libraries
 import importlib
 
+# Import required environment class
 class_import = {"PrimaVita": importlib.import_module('pygeneses.envs.prima_vita').PrimaVita}
 
 class HyperTune:
+    """
+    HyperTune class for hyperparameter tuning
+
+    Data members
+    ============
+    model_class    (str)
+        : Name of model environment to tune hyperparameters for
+    hyperparameter (str)
+        : Hyperparameter to tune
+    values         (list)
+        : Value list which should be checked with current hyperparameter
+    stop_at        (int)
+        : Approximate number of logs to be generated
+    """
 
     def __init__(self, model_class, hyperparameter, values, stop_at):
+        """
+        Initializer for HyperTune class
+
+        Params
+        ======
+        model_class    (str)
+            : Name of model environment to tune hyperparameters for
+        hyperparameter (str)
+            : Hyperparameter to tune
+        values         (list)
+            : Value list which should be checked with current hyperparameter
+        stop_at        (int)
+            : Approximate number of logs to be generated
+        """
+
         self.model_class = model_class
         self.hyperparameter = hyperparameter
         self.values = values
         self.stop_at = stop_at
 
     def hypertuner(self):
+        """
+        Runs environment with every value of hyperparameter specified
+        """
+
         for value in self.values:
             object = class_import[self.model_class](log_dir_info=self.hyperparameter + "_" + str(value))
             setattr(object, self.hyperparameter, value)
